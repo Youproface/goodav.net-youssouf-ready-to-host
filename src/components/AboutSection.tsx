d640b73c9ef3647b49186d9637b9d5eb2fb3901a
Youproface <hakizimanayoussouf@gmail.com>
2025-08-30 15:11:01 +0200

    Auto-commit: file change detected

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -24,7 +24,7 @@ export default function AboutUsSection() {
       y: 0,
       transition: {
         duration: 0.6,
-        ease: easeInOut
+        ease: [0.22, 1, 0.36, 1] as [number, number, number, number]
       }
     }
   };
