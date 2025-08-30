ae9571323ebf4a326ef0e156923ad91d2c7b5010
Youproface <hakizimanayoussouf@gmail.com>
2025-08-30 16:15:52 +0200

    Auto-commit: file change detected

--- src/components/forms/BookingModal.tsx
+++ src/components/forms/BookingModal.tsx
@@ -777,7 +777,7 @@ function Step5({ nextStep }: { nextStep: () => void }) {
 /* -------------------------
    STEP 6
 ------------------------- */
-function Step6({ submitStatus, handleFormSubmit, name, setName, email, setEmail, phone, setPhone, countryCode, setCountryCode, countryCodes, organization, setOrganization, project, setProject, selectedDate, setSelectedDate, selectedTime, setSelectedTime, timezone, setTimezone, submitting, handleTimeConfirmation, confirmationMessage, timeSlotConfirmed }) {
+function Step6({ submitStatus, handleFormSubmit, name, setName, email, setEmail, phone, setPhone, countryCode, setCountryCode, countryCodes, organization, setOrganization, project, setProject, selectedDate, setSelectedDate, selectedTime, setSelectedTime, timezone, setTimezone, submitting, handleTimeConfirmation, confirmationMessage, timeSlotConfirmed, setTimeSlotConfirmed }) {
   // Calendar and time slot picker UI
   // List of time zones (IANA)
   const timeZones = [
