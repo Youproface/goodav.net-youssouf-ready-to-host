4a56f0c9092ff198e121e056c9b99b170d619278
Youproface <hakizimanayoussouf@gmail.com>
2025-09-04 11:45:52 +0200

    Add .DS_Store to .gitignore to exclude macOS system files

--- src/components/services/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as VideoProduction } from './VideoProduction';
-export { default as Photography } from './Photography';
-export { default as AudioProduction } from './AudioProduction';
-export { default as SoundSystem } from './SoundSystem';
-export { default as Streaming } from './Streaming';
-export { default as Lighting } from './Lighting';
